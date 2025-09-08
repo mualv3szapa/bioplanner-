bf5a8e71e7ea3a3c20a3ebed1c9ca24afc95c262
mualv3szapa <murilo.zapiello@gmail.com>
2025-09-08 16:25:57 -0300

    fix: Regenerate package-lock.json to fix build dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const repoName = "bioplanner-";
-
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
-  basePath: isProd ? `/${repoName}` : undefined,
-  assetPrefix: isProd ? `/${repoName}/` : undefined,
   trailingSlash: true,
 };
 
